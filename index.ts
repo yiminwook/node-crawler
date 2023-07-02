1ba11c46869bcf594ec487fd18439d056ed03f05
yiminwook <grs0416@gmail.com>
2023-07-02 21:44:35 +0900

    Feat: xlsx parser

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { parse } from 'csv-parse';
 import fs from 'fs';
+import xlsx from 'xlsx';
 
 const records: [string, string][] = [];
 
@@ -10,6 +11,19 @@ fs.createReadStream('csv/data.csv')
   })
   .on('end', () => {
     records.forEach((r, i) => {
-      console.log(i, r);
+      // console.log(i, r);
     });
   });
+
+const workbook = xlsx.readFile('xlsx/data.xlsx');
+const sheet = workbook.Sheets['영화목록'];
+
+const data = xlsx.utils.sheet_to_json(sheet) as { 제목: string; 링크: string }[];
+
+data.forEach(({ 제목, 링크 }) => {
+  console.log(제목, 링크);
+});
+
+for (const [i, r] of data.entries()) {
+  console.log(r.제목, r.링크);
+}
